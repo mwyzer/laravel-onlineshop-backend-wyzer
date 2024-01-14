52f4cd248b5c47fe1be984f348f17172e449c098
mwyzer <muhammad.wyzer@gmail.com>
2024-01-14 21:48:30 +0700

    api auth

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//register
+Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
+// same as this
+// Route::post('/register', 'App\Http\Controllers\Api\AuthController@register');
+//logout
+Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
+//login
+Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+
